e69aa2838191e1f052be07e76cf757e3fdda1d62
Frederic,Tu <frdtu@paypal.com>
2017-08-20 23:10:49 +0800

    Add check bash-fwk and install it inside the VM if exist on the host

--- start-docker.bash
+++ start-docker.bash
@@ -1,6 +1,8 @@
 source scripts/dck-kafka.bash
 source scripts/dck-zk.bash
 
+export BASH_FWK_ROOT=~/git/bash-fwk
+
 instscript() {
 	echo "Install bash-fwk that contains Docker & Vagrant utilities."
 	echo "$(curl -fsSL https://raw.githubusercontent.com/frtu/bash-fwk/master/autoinstaller4curl.bash)" | bash
@@ -23,9 +25,20 @@ kafstartsimple() {
 }
 
 dckonvagrant() {
-	echo "Make sure you have ** vagrant ** installed !! If you have bash-fwk, use 'vagbadd_docker_ubuntu_trusty' to pre install image."
-	(cd vagrant-docker && exec vagrant up)
+	echo "Make sure you have ** vagrant ** installed !!"
+	echo "- If you have bash-fwk, use 'vagbadd_docker_ubuntu_trusty' to pre install image."
+	(cd vagrant-docker && exec vagrant up && exec dckonvagrantinit)
 }
+
+dckonvagrantinit() {
+  vagrant plugin install vagrant-docker-compose
+  if [[ -d $BASH_FWK_ROOT ]]; then
+    echo "== Since you've install 'bash-fwk' enable it inside the VM & add docker utilities =="
+    vaginst_script
+    vagenabledocker
+  fi
+}
+
 
 echo "Type 'instscript' to install bash-fwk locally"
 
