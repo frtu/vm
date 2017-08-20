bbcf3e43aa580b0c87ea218931f77d8409f0607a
Frederic,Tu <frdtu@paypal.com>
2017-08-20 22:22:43 +0800

    Add Docker on Vagrant flavor relying on Ubuntu VM

--- start-docker.bash
+++ start-docker.bash
@@ -1,7 +1,13 @@
 source scripts/dck-kafka.bash
 source scripts/dck-zk.bash
 
+instscript() {
+	echo "Install bash-fwk that contains Docker & Vagrant utilities."
+	echo "$(curl -fsSL https://raw.githubusercontent.com/frtu/bash-fwk/master/autoinstaller4curl.bash)" | bash
+}
+
 kafstartplatform() {
+	echo "Make sure you have ** docker-compose ** installed !!"
 	(cd kafka-platform && exec docker-compose up)
 	dckmport 2181
 	dckmport 9092
@@ -10,10 +16,20 @@ kafstartplatform() {
 	dckmport 8083
 }
 kafstartsimple() {
+	echo "Make sure you have ** docker-compose ** installed !!"
 	(cd kafka-simple && exec docker-compose up)
 	dckmport 2181
 	dckmport 9092
 }
 
+dckonvagrant() {
+	echo "Make sure you have ** vagrant ** installed !! If you have bash-fwk, use 'vagbadd_docker_ubuntu_trusty' to pre install image."
+	(cd vagrant-docker && exec vagrant up)
+}
+
+echo "Type 'instscript' to install bash-fwk locally"
+
 echo "Type 'kafstartsimple' to start kafka-simple"
 echo "Type 'kafstartplatform' to start kafka-platform"
+echo "Type 'dckonvagrant' to start Docker on Vagrant"
+
