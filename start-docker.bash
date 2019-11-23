e3227b6cc1fd8b75f6dc9fef0c889c45e35e0e57
Tu, Fred <frederic.tu@gmail.com>
2019-11-23 23:17:26 +0800

    Reorganize lines

--- start-docker.bash
+++ start-docker.bash
@@ -3,33 +3,18 @@ source scripts/dck-zk.bash
 
 export BASH_FWK_ROOT=~/git/bash-fwk
 
+echo "Type 'instscript' to install bash-fwk locally"
 instscript() {
 	echo "Install bash-fwk that contains Docker & Vagrant utilities."
 	echo "$(curl -fsSL https://raw.githubusercontent.com/frtu/bash-fwk/master/autoinstaller4curl.bash)" | bash
 }
 
-kafstartplatform() {
-	echo "Make sure you have ** docker-compose ** installed !!"
-	(cd kafka-platform && exec docker-compose up)
-	dckmport 2181
-	dckmport 9092
-	dckmport 8081
-	dckmport 8082
-	dckmport 8083
-}
-kafstartsimple() {
-	echo "Make sure you have ** docker-compose ** installed !!"
-	(cd kafka-simple && exec docker-compose up)
-	dckmport 2181
-	dckmport 9092
-}
-
+echo "Type 'dckonvagrant' to start Docker on Vagrant"
 dckonvagrant() {
 	echo "Make sure you have ** vagrant ** installed !!"
 	echo "- If you have bash-fwk, use 'vagbadd_docker_ubuntu_trusty' to pre install image."
 	(cd vagrant-docker && exec vagrant up && exec dckonvagrantinit)
 }
-
 dckonvagrantinit() {
   vagrant plugin install vagrant-docker-compose
   if [[ -d $BASH_FWK_ROOT ]]; then
@@ -39,10 +24,20 @@ dckonvagrantinit() {
   fi
 }
 
-
-echo "Type 'instscript' to install bash-fwk locally"
-
 echo "Type 'kafstartsimple' to start kafka-simple"
+kafstartplatform() {
+	echo "Make sure you have ** docker-compose ** installed !!"
+	(cd kafka-platform && exec docker-compose up)
+	dckmport 2181
+	dckmport 9092
+	dckmport 8081
+	dckmport 8082
+	dckmport 8083
+}
 echo "Type 'kafstartplatform' to start kafka-platform"
-echo "Type 'dckonvagrant' to start Docker on Vagrant"
-
+kafstartsimple() {
+	echo "Make sure you have ** docker-compose ** installed !!"
+	(cd kafka-simple && exec docker-compose up)
+	dckmport 2181
+	dckmport 9092
+}
