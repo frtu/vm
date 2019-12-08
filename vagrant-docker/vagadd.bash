0dce7f484bf364cc11342528bdb4504a4fdbf9f3
Tu, Fred <frederic.tu@gmail.com>
2019-12-08 11:01:31 +0800

    Allow to create base image 'vagrant-docker-centos7' and import / export into 'vagrant-docker-registry'

---
+++ vagrant-docker/vagadd.bash
@@ -0,0 +1,10 @@
+export() {
+  vagexport vagrant-docker-centos7 vagrant-docker-centos7.box
+  vagexportkeyvbox
+}
+import() {
+  vagbadd vagrant-docker-centos7 vagrant-docker-centos7.box
+
+  vagbls
+  echo "You can use vagrant image : vagrant-docker-centos7 don't forget to import private key with vagimportkeyvbox"
+}
