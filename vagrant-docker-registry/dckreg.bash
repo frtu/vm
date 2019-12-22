1acaa190d1550e6fdca59f6fcd910efc1a19aaa0
Tu, Fred <frederic.tu@gmail.com>
2019-12-22 13:39:07 +0800

    Rename ssl folder -> certs folder. Add reverse proxy into vagrant-docker-registry

--- vagrant-docker-registry/dckreg.bash
+++ vagrant-docker-registry/dckreg.bash
@@ -3,3 +3,14 @@ boxexport() {
   vagexport vagrant-docker-registry vagrant-docker-registry.box
   vagexportkeyvbox
 }
+
+echo "Type 'regcertgen' to generate key & cert for local server - ex: myregistry-127-0-0-1.nip.io"
+regcertgen() {
+  mkdir -p conf/registry/nginx/certs
+  (cd conf/registry/nginx/certs && gencertkey server)
+}
+
+echo "Type 'startnginx nginx' to start nginx using this folder in /etc/nginx"
+startnginx() {
+  docker run -d --name nginx-proxy -v $1:/etc/nginx/ -p 80:80 -p 443:443 -P nginx:alpine
+}
