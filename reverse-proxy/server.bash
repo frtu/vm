2f535b5ed9febdf65c503dabd522951cc2ebb98c
Tu, Fred <frederic.tu@gmail.com>
2019-12-22 14:31:00 +0800

    Expose IP4 & IP6 for 80 & 443

--- reverse-proxy/server.bash
+++ reverse-proxy/server.bash
@@ -1,5 +1,10 @@
-echo "Type 'regcertgen' to generate key & cert for local server - ex: reverse-proxy-127-0-0-1.nip.io"
-regcertgen() {
-  mkdir -p conf/nginx/ssl
-  (cd conf/nginx/ssl && gencertkey server)
+echo "Type 'nginxinit' to generate key & cert for local server - ex: reverse-proxy-127-0-0-1.nip.io"
+nginxinit() {
+  mkdir -p conf/nginx/certs
+  (cd conf/nginx/certs && gencertkey server)
+}
+
+echo "Type 'nginxstart' to start nginx using this folder in /etc/nginx"
+nginxstart() {
+  (cd conf && docker run -d --name nginx-proxy -v nginx:/etc/nginx/ -p 80:80 -p 443:443 -P nginx:alpine)
 }
