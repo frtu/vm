7c9d650f3346903f1a835cfb1fdce26d4aa07e5c
Tu, Fred <frederic.tu@gmail.com>
2019-12-22 14:20:24 +0800

    Remove nginx from docker-compose and let docker-registry directly expose 443 and use the certificates

--- vagrant-docker-registry/dckreg.bash
+++ vagrant-docker-registry/dckreg.bash
@@ -4,13 +4,8 @@ boxexport() {
   vagexportkeyvbox
 }
 
-echo "Type 'regcertgen' to generate key & cert for local server - ex: myregistry-127-0-0-1.nip.io"
-regcertgen() {
-  mkdir -p conf/registry/nginx/certs
-  (cd conf/registry/nginx/certs && gencertkey server)
-}
-
-echo "Type 'startnginx nginx' to start nginx using this folder in /etc/nginx"
-startnginx() {
-  docker run -d --name nginx-proxy -v $1:/etc/nginx/ -p 80:80 -p 443:443 -P nginx:alpine
+echo "Type 'reginit' to generate key & cert for local server - ex: myregistry-127-0-0-1.nip.io"
+reginit() {
+  mkdir -p conf/registry/certs
+  (cd conf/registry/certs && gencertkey server)
 }
