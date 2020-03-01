e19ade0b73921b28ec0b62feea4011ba59dd87d0
Tu, Fred <frederic.tu@gmail.com>
2020-03-01 22:07:43 +0800

    Allow to build Docker image for nginx-bridge

--- nginx-bridge/server.bash
+++ nginx-bridge/server.bash
@@ -4,7 +4,17 @@ nginxinit() {
   (cd conf/nginx/certs && gencertkey server)
 }
 
-echo "Type 'nginxstart' to start nginx using this folder in /etc/nginx"
+echo "Type 'nginxstart' to start nginx using this folder in ./conf/nginx/"
 nginxstart() {
   (cd conf && docker run -d --name nginx-proxy -v nginx:/etc/nginx/ -p 80:80 -p 443:443 -P nginx:alpine)
+}
+
+echo "Type 'nginxbuild' to build nginx using this folder in ./conf/nginx/"
+nginxbuild() {
+  # MIN NUM OF ARG
+  if [[ "$#" < "1" ]]; then
+      echo "Usage : nginxbuild IMAGE_NAME" >&2
+      return 1
+  fi
+  docker build -t $1 .
 }
