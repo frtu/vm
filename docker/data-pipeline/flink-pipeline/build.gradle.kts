884c549980fb2c64ce0c7469163de4f8e585b17e
Tu, Fred <frederic.tu@gmail.com>
2025-10-05 23:54:39 +0800

    feat: docker data-pipeline / flink-pipeline : Enable Spring Boot Kafka & initialise KafkaSource & WordCount app

--- docker/data-pipeline/flink-pipeline/build.gradle.kts
+++ docker/data-pipeline/flink-pipeline/build.gradle.kts
@@ -40,7 +40,9 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-configuration-processor")
+    implementation("org.springframework.kafka:spring-kafka")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    // testImplementation("org.springframework.kafka:spring-kafka-test")
 
     // core
     implementation(libs.jackson.kotlin)
--- docker/data-pipeline/flink-pipeline/src/main/kotlin/com/github/frtu/vm/sample/embedded/application/FlinkAppConfiguration.kt
+++ docker/data-pipeline/flink-pipeline/src/main/kotlin/com/github/frtu/vm/sample/embedded/application/FlinkAppConfiguration.kt
@@ -1,6 +1,14 @@
 package com.github.frtu.vm.sample.embedded.application
 
+import com.github.frtu.vm.sample.standalone.WordCount
+import org.apache.flink.api.common.serialization.SimpleStringSchema
+import org.apache.flink.api.connector.source.Source
+import org.apache.flink.connector.kafka.source.KafkaSource
+import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment
+import org.apache.flink.streaming.api.functions.sink.PrintSink
 import org.slf4j.LoggerFactory
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean
@@ -13,6 +21,32 @@ class FlinkAppConfiguration {
     @Bean
     @ConfigurationProperties(prefix = "application.flink.default")
     fun flinkAppProperties() = FlinkAppProperties()
+
+    @Bean
+    fun kafkaSource(
+        kafkaProperties: KafkaProperties,
+        flinkAppProperties: FlinkAppProperties,
+    ): Source<String, *, *> {
+        val topic = flinkAppProperties.source?.topic
+        logger.debug("TOPIC:{} - KAFKA:{}", topic, kafkaProperties.buildProducerProperties())
+        return KafkaSource.builder<String>()
+            .setBootstrapServers(kafkaProperties.bootstrapServers.first())
+            .setTopics(topic)
+            .setStartingOffsets(OffsetsInitializer.earliest())
+            .setValueOnlyDeserializer(SimpleStringSchema())
+            .build()
+    }
+
+    @Bean
+    fun registerApplication(
+        env: StreamExecutionEnvironment,
+        flinkAppProperties: FlinkAppProperties,
+        source: Source<String, *, *>,
+    ): String {
+        val wordCount = WordCount()
+        wordCount.defineWorkflow(env, source, 1) { workflow -> workflow.sinkTo(PrintSink()) }
+        return "OK"
+    }
 
     private val logger = LoggerFactory.getLogger(this::class.java)
 }
