f01ec7167b051c03cc0cbc354621e8d725651847
Tu, Fred <frederic.tu@gmail.com>
2025-10-05 22:33:14 +0800

    chore: docker data-pipeline : test collect data in memory

--- docker/data-pipeline/flink-pipeline/src/test/kotlin/com/github/frtu/vm/sample/embedded/FlinkStreamingSpringBootTest.kt
+++ docker/data-pipeline/flink-pipeline/src/test/kotlin/com/github/frtu/vm/sample/embedded/FlinkStreamingSpringBootTest.kt
@@ -3,11 +3,14 @@ package com.github.frtu.vm.sample.embedded
 import io.kotest.matchers.shouldBe
 import java.nio.file.Files
 import java.nio.file.Paths
+import java.util.concurrent.ConcurrentLinkedQueue
 import org.apache.flink.api.common.typeinfo.TypeInformation
 import org.apache.flink.api.common.typeutils.base.IntSerializer
 import org.apache.flink.core.fs.FileSystem
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment
+import org.apache.flink.streaming.api.functions.sink.SinkFunction
 import org.apache.flink.streaming.api.functions.source.FromElementsFunction
+import org.apache.flink.streaming.connectors.kafka.table.SinkBufferFlushMode
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
@@ -16,30 +19,40 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Import
 import org.springframework.context.annotation.Primary
 
+class CollectorSink: SinkFunction<Int> {
+    override fun invoke(value: Int, context: SinkFunction.Context) {
+        results.add(value)
+        super.invoke(value, context)
+    }
+
+    companion object {
+        val results = ConcurrentLinkedQueue<Int>()
+        fun clear() = results.clear()
+    }
+}
+
 @SpringBootTest(classes = [FlinkStreamingSpringBoot::class])
 @Import(
-    FlinkStreamingSpringBootTestPropertiesConfiguration::class,
     FlinkStreamingSpringBootTestConfiguration::class,
 )
 class FlinkStreamingSpringBootTest {
     @Autowired
     lateinit var flinkProperties: FlinkProperties
 
-    @Autowired
-    lateinit var outputFileName: String
+//    @Autowired
+//    lateinit var outputFileName: String
 
     @Test
     fun localStreamExecution() {
+        CollectorSink.clear()
         Thread.sleep(flinkProperties.terminationGracePeriodMs / 2) // fixme
-        val outputFileText = String(Files.readAllBytes(Paths.get(outputFileName))).trim { it <= ' ' }
-        outputFileText shouldBe "2"
-    }
-}
+//        val result = String(Files.readAllBytes(Paths.get(outputFileName))).trim { it <= ' ' }
+        val output = CollectorSink.results.toList()
+        output.size shouldBe 1
 
-@TestConfiguration
-class FlinkStreamingSpringBootTestPropertiesConfiguration {
-    @Bean
-    fun outputFileName(): String = "build/FlinkStreamingSpringBootTest.txt"
+        val result = output[0]
+        result shouldBe 2
+    }
 }
 
 @TestConfiguration
@@ -49,14 +62,11 @@ class FlinkStreamingSpringBootTestConfiguration {
     fun getFlinkEnvironment(flinkProperties: FlinkProperties) = StreamExecutionEnvironment.createLocalEnvironment()
 
     @Autowired
-    fun populateEnv(flinkEnvironment: StreamExecutionEnvironment, outputFileName: String) {
+    fun populateEnv(flinkEnvironment: StreamExecutionEnvironment) {
         flinkEnvironment
-            .addSource(
-                FromElementsFunction(IntSerializer(), 1, 2, 3),
-                TypeInformation.of(Int::class.java)
-            )
+            .fromElements(1, 2, 3)
             .filter { i: Int -> i % 2 == 0 }
-            .writeAsText(outputFileName, FileSystem.WriteMode.OVERWRITE)
+            .addSink(CollectorSink())
             .setParallelism(1)
     }
 }
